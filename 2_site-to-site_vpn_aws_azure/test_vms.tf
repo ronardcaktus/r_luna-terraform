cc43d294e8640e299d3071168218372a3a9cac8d
ronardcaktus <rluna@caktusgroup.com>
2025-04-26 09:41:49 -0400

    Run format command

--- 2_site-to-site_vpn_aws_azure/test_vms.tf
+++ 2_site-to-site_vpn_aws_azure/test_vms.tf
@@ -16,7 +16,7 @@
 #   name                = "network_interface_vm"
 #   location            = azurerm_resource_group.resource_group.location
 #   resource_group_name = azurerm_resource_group.resource_group.name
-  
+
 #   ip_configuration {
 #     name                          = "internal"
 #     subnet_id                     = azurerm_subnet.subnet_1.id
